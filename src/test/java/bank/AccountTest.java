3508785fb744f71e0118ebc7255090771872de25
CarlKlagba <klagba@gmail.com>
2018-01-06 15:57:23 +0100

    add accout unit test

--- src/test/java/bank/AccountTest.java
+++ src/test/java/bank/AccountTest.java
@@ -13,7 +13,7 @@ public class AccountTest {
 		
 		account.deposit(1.0);
 		
-		assertEquals(1.0, account.getAmount(), 0);
+		assertEquals(1.0, account.getAmount(), 0.009);
 	}
 	
 	@Test
@@ -22,7 +22,16 @@ public class AccountTest {
 		
 		account.deposit(47.12);
 		
-		assertEquals(5559.46, account.getAmount(), 0);
+		assertEquals(5559.46, account.getAmount(), 0.009);
+	}
+	
+	@Test
+	public void deposit_in_deficitAccount() throws Exception {
+		Account account = new Account(-70.0);
+		
+		account.deposit(90.9);
+		
+		assertEquals(20.9, account.getAmount(), 0.009);
 	}
 	
 	@Test(expected = ForbidenTransaction.class)
